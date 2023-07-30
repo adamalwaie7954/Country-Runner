2cfc869ad55d97924d31d34f99a4cd2079c5bb36
DESKTOP-NQQS146\quart <kaynchan123@gmail.com>
2023-07-30 16:19:17 +0800

    a

--- Assets/Scripts/movewithcam.cs
+++ Assets/Scripts/movewithcam.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 public class movewithcam : MonoBehaviour
 {
     public GameObject go;
+    public GameObject spawn1;
+    public GameObject bg;
     void LateUpdate()
     {
         //attach Game Object go to target
         Transform target = Camera.main.transform;
         go.transform.parent = target;
+        spawn1.transform.parent = target;
+        bg.transform.parent = target;
     }
 }
